f54b5cc646abf1ac7751b1a7e6de75777e96bf24
Fabrizzioperilli <alu0101138589@ull.edu.es>
2024-05-20 11:47:30 +0100

    Se corrigen los estilos de la pestaña recomendaciones del perfil alumnado

--- components/alumnado/recomendador_alumnado.py
+++ components/alumnado/recomendador_alumnado.py
@@ -17,21 +17,21 @@ def recomendador_alumnado():
         html.P("Existen muchas razones por las que un estudiante puede abandonar sus estudios universitarios. Algunas de las "
                "razones más comunes son:", className='p-recomendador-alumnado'),
         html.Ul([
-            html.Li("Problemas económicos: Muchos estudiantes abandonan la universidad por problemas económicos. La falta de "
+            html.Li(html.Span([html.Strong("Problemas económicos:"), " Muchos estudiantes abandonan la universidad por problemas económicos. La falta de "
                     "recursos económicos puede dificultar el acceso a la educación superior y hacer que los estudiantes abandonen "
-                    "sus estudios.", className='li-recomendador-alumnado'),
-            html.Li("Problemas académicos: Los problemas académicos también pueden ser una causa de deserción universitaria. "
+                    "sus estudios."]), className='li-recomendador-alumnado'),
+            html.Li(html.Span([html.Strong("Problemas académicos:"), " Los problemas académicos también pueden ser una causa de deserción universitaria. "
                     "Los estudiantes que tienen dificultades para aprobar las asignaturas pueden sentirse frustrados y abandonar "
-                    "la universidad.", className='li-recomendador-alumnado'),
-            html.Li("Problemas personales: Los problemas personales, como la salud, la familia o las relaciones personales, pueden "
+                    "la universidad."]), className='li-recomendador-alumnado'),
+            html.Li(html.Span([html.Strong("Problemas personales:"), " Los problemas personales, como la salud, la familia o las relaciones personales, pueden "
                     "ser una causa de deserción universitaria. Los estudiantes que tienen problemas personales pueden tener dificultades "
-                    "para concentrarse en sus estudios y pueden abandonar la universidad.", className='li-recomendador-alumnado'),
-            html.Li("Falta de motivación: La falta de motivación es otra causa común de deserción universitaria. Los estudiantes que "
-                    "no están motivados para estudiar pueden abandonar la universidad.", className='li-recomendador-alumnado'),
-            html.Li("Falta de orientación: La falta de orientación también puede ser una causa de deserción universitaria. Los estudiantes "
+                    "para concentrarse en sus estudios y pueden abandonar la universidad."]), className='li-recomendador-alumnado'),
+            html.Li(html.Span([html.Strong("Falta de motivación:"), " La falta de motivación es otra causa común de deserción universitaria. Los estudiantes que "
+                    "no están motivados para estudiar pueden abandonar la universidad."]), className='li-recomendador-alumnado'),
+            html.Li(html.Span([html.Strong("Falta de orientación:"), " La falta de orientación también puede ser una causa de deserción universitaria. Los estudiantes "
                     "que no reciben la orientación adecuada pueden tener dificultades para adaptarse a la vida universitaria y pueden "
-                    "abandonar la universidad.", className='li-recomendador-alumnado'),
-        ]),
+                    "abandonar la universidad."]), className='li-recomendador-alumnado'),
+        ], className='ul-recomendador-alumnado'),
         html.H2("¿Cómo prevenir la deserción universitaria?", className='sb-recomendador-alumnado'),
         html.P("Para prevenir la deserción universitaria, las instituciones educativas deben implementar estrategias de atracción "
                "y retención de estudiantes. Algunas de las estrategias más efectivas para prevenir la deserción universitaria son:", className='p-recomendador-alumnado'),
@@ -41,5 +41,5 @@ def recomendador_alumnado():
             html.Li("Ofrecer programas de orientación y asesoramiento a los estudiantes para ayudarles a adaptarse a la vida universitaria.", className='li-recomendador-alumnado'),
             html.Li("Ofrecer programas de formación y capacitación a los docentes para mejorar la calidad de la enseñanza.", className='li-recomendador-alumnado'),
             html.Li("Ofrecer programas de seguimiento y evaluación a los estudiantes para identificar a tiempo los problemas y prevenir la deserción.", className='li-recomendador-alumnado'),
-        ])
+        ], className='ul-recomendador-alumnado'),
     ])
