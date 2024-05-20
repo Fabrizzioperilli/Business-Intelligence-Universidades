fd6c067e131a3540beae460320e0d9bbfdd83f13
Fabrizzioperilli <alu0101138589@ull.edu.es>
2024-05-20 12:10:37 +0100

    Cambios en el gráfico nota cuantitativa por cada asignatura, perfil docente

--- callbacks/alumnado/graphs/personal/callback_graph_calif_numerica_alumnado.py
+++ callbacks/alumnado/graphs/personal/callback_graph_calif_numerica_alumnado.py
@@ -1,7 +1,7 @@
 from dash import Input, Output, callback
 import plotly.graph_objs as go
 from data.queries import calif_numerica_asignatura
-from utils.utils import list_to_tuple
+from utils.utils import list_to_tuple, random_color
 
 @callback(
     Output('graph-bar-calificaciones-por-asignatura', 'figure'),
@@ -31,15 +31,29 @@ def update_graph_alumnado(alumno_id, curso_academico, titulacion):
         subjects.append(subject)
         grades.append(grade)
 
-    trace = go.Bar(x=subjects, y=grades, marker_color='blue', opacity=0.7)
+    colors = random_color(len(subjects))
+
+    # Crear una barra para cada asignatura con su propio color
+    traces = []
+    for subject, grade, color in zip(subjects, grades, colors):
+        traces.append(go.Bar(x=[subject], y=[grade], name=subject, marker_color=color, opacity=0.8))
 
     layout = go.Layout(
         title={'text': 'Calificación cuantitativa de las asignaturas matriculadas del alumno', 'x': 0.5},
         xaxis={'title': 'Asignatura', 'tickangle': 45},
         yaxis={'title': 'Calificación'},
-        height=600
+        height=600,
+        showlegend=True,
+        legend=dict(
+            x=1,
+            y=1,
+            traceorder='normal',
+            font=dict(
+                size=10,  # Ajusta el tamaño de la fuente para que sea más pequeño
+            ),
+            title='Asignaturas'
+        )
     )
 
-    figure = go.Figure(data=[trace], layout=layout)
+    figure = go.Figure(data=traces, layout=layout)
     return figure
-
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,8 @@
 from data.queries import nota_media_alumno_titulacion
+import random
 
+#Función para calcular la media de los alumnos
 def calculate_average_grade(alumno_id, titulacion):
-    
     result = nota_media_alumno_titulacion(alumno_id, titulacion)
     
     if result and result[0][0] is not None:
@@ -9,7 +10,7 @@ def calculate_average_grade(alumno_id, titulacion):
     else:
         return "No disponible"
     
-
+#Función para convertir una lista en una tupla
 def list_to_tuple(value):
     if isinstance(value, str):
         return (value,)
@@ -17,3 +18,8 @@ def list_to_tuple(value):
         return tuple(value)
     else:
         return ValueError("Tipo de dato no soportado, se esperaba un list o str. {}".format(type(value)))
+    
+#Función para generar colores aleatorios
+def random_color(size):
+    return ['#%06X' % random.randint(0, 0xFFFFFF) for _ in range(size)]
+    
